14785cdebc31e3daeff5d8ce0bd105f9df43e5bc
wang <1442963504@qq.com>
2017-01-18 14:47:28 +0800

    工程接入测试controller

--- src/main/java/com/ioe/zhy/controller/project/ProjectController.java
+++ src/main/java/com/ioe/zhy/controller/project/ProjectController.java
@@ -4,6 +4,7 @@ import com.ioe.zhy.entity.Project;
 import com.ioe.zhy.service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -19,10 +20,29 @@ public class ProjectController {
     ProjectService projectService;
 
     @RequestMapping("/add")
+    @ResponseBody
     public Map add(Project project){
         projectService.saveProjectRecord(project.getCategory(),project.getContent(),project.getResult(),project.isStatus(),project.getPowerClient_id(),project.getElectrician_id(),project.getService_company_id());
         Map<String ,Object> result=new HashMap<>();
         result.put("code","0");
+        return result;
+    }
+
+    @RequestMapping("/update")
+    @ResponseBody
+    public Map update(Project project){
+        projectService.updateProjectRecord(project.getRecord_id(),project.getCategory(),project.getContent(),project.getResult(),project.isStatus(),project.getPowerClient_id(),project.getElectrician_id(),project.getService_company_id());
+        Map<String ,Object> result=new HashMap<>();
+        result.put("code","0");
+        return result;
+    }
+
+    @RequestMapping("/get")
+    @ResponseBody
+    public Map get(String powerClient_id){
+        Map<String ,Object> result=new HashMap<>();
+        result.put("code","0");
+        result.put("data",projectService.getProjectRecord(powerClient_id));
         return result;
     }
 }
