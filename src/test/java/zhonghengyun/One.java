75b49c439642a58babc87053392019b32676d0cd
huangqian10 <450479001@qq.com>
2017-01-19 17:51:01 +0800

    1.0.7

---
+++ src/test/java/zhonghengyun/One.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package zhonghengyun;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ioe.zhy.dao.ElectricianDao;
+
+/**
+ * @author hq
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:spring/spring-mybatis.xml"})
+public class One {
+	
+	   @Resource
+	    private ElectricianDao electricianDao;
+	
+	@Test
+	public void test(){
+		electricianDao.deleteElectricianByArea("e");
+		electricianDao.electricianAndArea("zhy/t_AssetAccountService:1484797223780", "314", null);
+	}
+	  
+
+
+}
